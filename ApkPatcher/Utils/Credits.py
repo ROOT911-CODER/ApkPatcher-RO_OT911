f01ee3827ecfae36a7a6ebda3932f965a3225932
iopmnb023-creator <iopmnb023@gmail.com>
2026-09-13 06:51:33 -0700

    Add files via upload

---
+++ ApkPatcher/Utils/Credits.py
@@ -0,0 +1,28 @@
+from ..ANSI_COLORS import ANSI; C = ANSI()
+
+
+# ---------------- Credits ----------------
+def Credits():
+    exit(f"""
+                     💢  Credit  💢
+
+{C.S} Flutter SSL & TG Patch {C.E}{C.G} 🇮🇳 AbhiTheM0dder 🇮🇳 {C.S}{C.P} @AbhiTheM0dder {C.E}
+
+{C.S} Pine Hook {C.E}{C.G} 🇮🇳 AbhiTheM0dder 🇮🇳 | 残页 {C.S}{C.P} @AbhiTheM0dder | @canyie {C.E}
+
+{C.S} MITM {C.E}{C.G} Niklas Higi {C.S}{C.P} https://github.com/shroudedcode/apk-mitm {C.E}
+
+{C.S} APKTool {C.E}{C.G} Connor Tumbleson {C.S}{C.P} @iBotPeaches {C.E}
+
+{C.S} APKEditor {C.E}{C.G} REAndroid {C.S}{C.P} @kikfox {C.E}
+
+{C.S} ApkSig {C.E}{C.G} Android Tools Build {C.S}{C.P} Open Source {C.E}
+
+{C.S} My Channel {C.E}{C.CC} {C.OG}RO_OT911 {C.CC}࿗ 🇮🇳 {C.S}{C.P} @RO_OT911 {C.E}
+
+{C.S} CREATOR {C.E}{C.G} 𓄂 Ꭱꫝℑ 𓆐 ︻デ═一 ࿗ Я͓̽K͓̽ ࿗ {C.S}{C.P} @RO_OT911 {C.E}
+
+
+{C.S}  NOTE  {C.E} {C.Y} Please Maintain Our Credits. 🙏🙏
+
+    """)
